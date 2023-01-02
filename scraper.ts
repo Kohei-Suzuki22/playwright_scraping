bd7e456e73bf15c403bf62a8555339b95aab9c45
kohei <bhkbb0551@gmail.com>
2023-01-02 14:56:25 +0900

    [feat] ブラウザにCSP無効の設定とランダムなuserAgentを設定

--- scraper.ts
+++ scraper.ts
@@ -1,25 +1,35 @@
 const playwright = require('playwright')
+const random_useragent = require('random-useragent')
 
 const BASE_URL = 'https://github.com/topics/playwright'
 
 ;(async () => {
-  
+  /**
+   * ーザーエージェント（User Agent：UA）は、「ネット利用者が使用しているOS・ブラウザ」のことを指す。
+   * 一般的なインターネットブラウザを使い、HTTPに基づきサイトなどにアクセスした際には、
+   * ユーザーエージェントに関する各種情報が、相手側に通知される仕組みとなっている。
+   *
+   * https://webtan.impress.co.jp/g/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%82%A8%E3%83%BC%E3%82%B8%E3%82%A7%E3%83%B3%E3%83%88
+   * */
+  const agent = random_useragent.getRandom()
+  console.log('agent', agent)
+
   // ブラウザのセットアップ
 
   const browser = await playwright.chromium.launch({ headless: false })
 
   /**
    * { bypassCSP: true } : CSPを無効にする。
-   * 
-   * CSPとは、ブラウザーが実行可能なスクリプトの有効なソースとみなすドメインを指定することにより、 XSS の発生する箇所を削減・根絶することができる。 
+   *
+   * CSPとは、ブラウザーが実行可能なスクリプトの有効なソースとみなすドメインを指定することにより、 XSS の発生する箇所を削減・根絶することができる。
    * CSP に対応したブラウザーは、サーバーから指定された許可リストに載っているドメインのスクリプトのみ実行し、他のスクリプトはすべて無視します。
-   * 
+   *
    * つまり、ブラウザ側で指定したスクリプトのみ実行できるような制限がCSP。
-   * 
+   *
    * https://developer.mozilla.org/ja/docs/Web/HTTP/CSP
    */
 
-  const context = await browser.newContext({ bypassCSP: true })
+  const context = await browser.newContext({ bypassCSP: true ,userAgent: agent})
   const page = await context.newPage()
   await page.setDefaultTimeout(30000)
   await page.setViewportSize({ width: 800, height: 600 })
